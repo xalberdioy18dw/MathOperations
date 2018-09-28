ab121ef9713c3308d7b92ecd43e6072dde5f4180
ik013043z1 <ik013043z1@H013043.NET>
2018-09-28 10:42:37 +0200

    first commit

---
+++ src/MathOperation.java
@@ -0,0 +1,16 @@
+
+public class MathOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 0;
+		a = 5;
+		a++;
+		a*=2;
+		a-=3;
+		a%=5;
+		a=a+7;
+		System.out.println("The value of a is:" + a);
+	}
+
+}
